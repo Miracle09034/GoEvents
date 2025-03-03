fb1fe557078219109cfeab1fa350f7f3d823ae4f
Miracle09034 <miracleAudu766@gmail.com>
2025-03-03 02:37:16 +0100

    Create Home.tsx

---
+++ components/Home.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import CategoryFilter from '@/components/shared/CategoryFilter';
+import Collection from '@/components/shared/Collection';
+import Search from '@/components/shared/Search';
+import { Button } from '@/components/ui/button';
+import { SearchParamProps } from '@/types';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface HomeProps {
+  events: any; // Replace `any` with the correct type for your events
+  searchParams: SearchParamProps;
+}
+
+export default function Home({ events, searchParams }: HomeProps) {
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10 mt-8">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">Celebrate Life's Precious Moments With Us!</h1>
+            <p className="p-regular-20 md:p-regular-24">
+              Book and create unforgettable events in Nigeria on GoEvents.
+            </p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">Explore Now</Link>
+            </Button>
+          </div>
+          <Image
+            src="/assets/images/hero.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+          />
+        </div>
+      </section>
+
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">
+          Lets Get <br /> The Party Started!
+        </h2>
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          <Search />
+          <CategoryFilter />
+        </div>
+        <Collection
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={searchParams?.page ? Number(searchParams.page) : 1}
+          totalPages={events?.totalPages}
+        />
+      </section>
+    </>
+  );
+}
