0d09d837f76bae383faa6202d54d70a11c4d821a
Miracle09034 <miracleAudu766@gmail.com>
2025-03-04 03:01:24 +0100

    Update page.tsx

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,20 +1,20 @@
-import { auth } from '@clerk/nextjs/server';
+import { getAuth } from '@clerk/nextjs/server';
 import ProfilePage from '@/components/ProfilePage'; // Import the client component
 import { getEventsByUser } from '@/lib/actions/event.actions';
 import { getOrdersByUser } from '@/lib/actions/order.actions';
 import { SearchParamProps } from '@/types';
+import { NextApiRequest } from 'next';
 
-export default async function Profile({ searchParams }: SearchParamProps) {
-  // Get the authenticated user object
-  const { userId, currentUser } = auth();
-  console.log(userId)
-  console.log(currentUser)
+export default async function Profile({ searchParams, req }: SearchParamProps & { req: NextApiRequest }) {
+  // Get the authenticated user object using `getAuth`
+  const { userId, user } = getAuth(req);
+  console.log(user)
   if (!user) {
     throw new Error("User not authenticated");
   }
 
   // Extract the `userId` from `publicMetadata`
-  const publicMetadataUserId = currentUser.publicMetadata.userId as string;
+  const publicMetadataUserId = user.publicMetadata.userId as string;
 
   // Fetch orders and events for the user
   const ordersPage = Number(searchParams?.ordersPage) || 1;
