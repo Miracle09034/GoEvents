9399cca7d08c4407e5ca847fd15a971395216251
Miracle09034 <miracleAudu766@gmail.com>
2025-03-07 02:49:16 +0100

    Update ProfilePage.tsx

--- components/ProfilePage.tsx
+++ components/ProfilePage.tsx
@@ -28,7 +28,7 @@ const ProfilePage = ({
     <>
       {/* My Tickets */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10" style={{ marginTop: '5rem', '@media (min-width: 640px)': { marginTop: '0' } }}>
-  {/* Your content */} >
+  {/* Your content */} 
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
           <Button asChild size="lg" className="button hidden sm:flex">
