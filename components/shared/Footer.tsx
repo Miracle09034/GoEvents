8af7c47c01afa7be2088c96bad8c2fa8678903b4
Miracle09034 <miracleAudu766@gmail.com>
2025-03-10 21:12:01 +0100

    Update Footer.tsx

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,13 +1,21 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+
+
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { footerLinks } from '@/constants'; // Import the footerLinks array
+import { usePathname } from 'next/navigation'
+
 
 const Footer = () => {
+    const pathname = usePathname() //to get the path name (this would help us figure out which is active)
+  
   return (
     <footer className='border-t'>
-      <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row '>
+      <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
         <Link href={'/'}>
-          <Image src={'/assets/images/logo.svg'}
+          <Image
+            src={'/assets/images/logo.svg'}
             alt='menu'
             width={128}
             height={28}
@@ -15,10 +23,31 @@ const Footer = () => {
           />
         </Link>
 
-        <p>2025 GoEvents. Created by Miracle </p>
+        {/* Footer Links */}
+        <ul className='flex flex-col gap-4 sm:flex-row sm:gap-8'>
+          {footerLinks.map((link) => {
+            const isActive = link.route === pathname
+            return (
+            <li key={link.route}>
+              <Link
+                href={link.route}
+                className={`${isActive && 'text-primary-500'}  p-regular-16 whitespace-nowrap hover:text-primary-500`}
+              >
+                {link.label}
+              </Link>
+            </li> )
+          })}
+        </ul>
+
+        <p>2025 GoEvents. Created by Miracle</p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
+
+
+
+
+
